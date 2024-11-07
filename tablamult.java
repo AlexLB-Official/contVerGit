b4ef51e7fb89a18e19814b5e13469f6b07036e24
Yoel <yoel4352@gmail.com>
2024-11-07 13:04:48 +0100

    He realizado el bucle de las tablas de multiplicar

---
+++ tablamult.java
@@ -0,0 +1,11 @@
+
+public class tablamult 
+{
+    public static void main (String args [])
+    {
+        for (int i =1; i <= 10; i++)
+        {
+            System.out.println("Tabla de mutliplicar de " + i);
+        }
+    }
+}
