2d8adfced4ce4771d8f8285e16b0eba153d39869
OScar <oscarcg3a@gmail.com>
2024-11-07 13:12:28 +0100

    programacion estructurada tablas multiplicar

--- tablamult.java
+++ tablamult.java
@@ -6,6 +6,10 @@ public class tablamult
         for (int i =1; i <= 10; i++)
         {
             System.out.println("Tabla de mutliplicar de " + i);
+            for (int j = 1; j <= 10; j++)
+            {
+                System.out.println(i + " x " + j + " = " + (i*j));
+            }
         }
     }
 }
